fcbcba60b2d0e99a335e6f020912a82bbf9cb425
temanhr1-sudo <teman.hr1@gmail.com>
2026-03-06 10:41:01 +0700

    update 3

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -3,4 +3,11 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+// Tambahkan log ini untuk memastikan variabel terbaca di console browser
+console.log("Supabase URL:", supabaseUrl); 
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.error("EROR: Variabel Supabase tidak ditemukan di .env!");
+}
+
+export const supabase = createClient(supabaseUrl || "", supabaseAnonKey || "")
